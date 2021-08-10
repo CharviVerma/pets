8b44d407ffe1f73fcb657946454e39d23e47af18
thatguyaneeket <aneeket1071@gmail.com>
2021-08-10 17:21:08 +0530

    Modal shows up on the same page

--- pets.js
+++ pets.js
@@ -1,2 +1,16 @@
+'use strict';
 
+const showUpFormBtn = document.querySelector('.account-link');
+const hiddenStuff = document.querySelectorAll('.hidden');
+const modelEl = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.cls-modal');
 
+const toggleModal = function () {
+  hiddenStuff.forEach((el) => {
+    el.classList.toggle('hidden');
+  });
+};
+
+showUpFormBtn.addEventListener('click', toggleModal);
+
+closeModalBtn.addEventListener('click', toggleModal);
